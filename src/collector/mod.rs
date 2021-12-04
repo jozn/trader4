69a58701f8b917c3483bc14fe74fee2d8a018a22
Hamid <7atashfeshan@gmail.com>
2021-12-04 13:21:37 +0330

    Improve collector

--- src/collector/mod.rs
+++ src/collector/mod.rs
@@ -3,6 +3,7 @@ use crate::ctrader::*;
 use crate::pb;
 use crate::pb::TickData;
 use std::fs;
+use std::sync::Arc;
 
 pub fn get_ticks() {
     let cfg = Config {
@@ -13,21 +14,26 @@ pub fn get_ticks() {
         client_token: "l4jT24BWu3etFSEVViQKu1NsGpBYf2nKN0DyUGgqjy0".to_string(),
         ctid: 22851452,
     };
-    let (mut cti, rc_event) = CTrader::connect(&cfg);
-    // let mut ct = cti.lock().unwrap();
-    let mut ct = cti;
-    ct.application_auth_req(&cfg.client_id, &cfg.client_secret);
-
-    std::thread::sleep(std::time::Duration::new(2, 0));
 
     let d = 1636317000_000;
-    let de = d + 1 * 86400_000;
-    ct.get_bid_tick_data_req(1, d, de);
-    // ct.get_ask_tick_data_req(1, d, de);
+    let de = d + 7 * 86400_000;
+
+    collect_data_from_api(&cfg, 1, d, de);
+}
+
+fn collect_data_from_api(cfg: &Config, symbol_id: i64, time_ms: i64, to_time_ms: i64) {
+    let start_time = time_ms;
+    let mut time_ms = time_ms;
 
     let mut collector = Collector::new();
-    let mut done_asks = false;
-    // event handling
+    let mut in_bids = true;
+
+    // Connect to cTrader server
+    let (mut ct, rc_event) = CTrader::connect(cfg);
+    ct.application_auth_req(&cfg.client_id, &cfg.client_secret);
+    std::thread::sleep(std::time::Duration::new(2, 0));
+    ct.get_bid_tick_data_req(symbol_id, time_ms, to_time_ms);
+
     for e in rc_event {
         match e.clone() {
             _ => {
@@ -37,52 +43,39 @@ pub fn get_ticks() {
 
         match e {
             ResponseEvent::Refresh => {
-                println!("EVENT");
+                // println!("EVENT");
             }
-            ResponseEvent::ApplicationAuthRes(_) => {}
-            ResponseEvent::AccountAuthRes(_) => {}
-            ResponseEvent::VersionRes(_) => {}
-            ResponseEvent::AssetListRes(_) => {}
-            ResponseEvent::SymbolsListRes(_) => {}
-            ResponseEvent::SymbolByIdRes(_) => {}
-            ResponseEvent::SymbolsForConversionRes(_) => {}
-            ResponseEvent::TraderRes(_) => {}
-            ResponseEvent::SubscribeSpotsRes(_) => {}
-            ResponseEvent::UnsubscribeSpotsReq(_) => {}
-            ResponseEvent::SpotEvent(_) => {}
-            ResponseEvent::DealListRes(_) => {}
-            ResponseEvent::GetTrendbarsRes(e) => {}
             ResponseEvent::ErrorRes(_) => {}
             ResponseEvent::GetTickDataRes(r) => {
-                if !done_asks {
-                    let ts = trans_ticks(&r.tick_data);
-                    collector.bids = ts;
-                    done_asks = true;
-                    ct.get_ask_tick_data_req(1, d, de);
+                let ts = trans_ticks(&r.tick_data);
+                if in_bids {
+                    // bids
+                    ts.iter().for_each(|v| collector.bids.push(v.clone()));
+                    if r.has_more {
+                        time_ms = ts.last().unwrap().timestamp + 1;
+                        ct.get_bid_tick_data_req(symbol_id, time_ms, to_time_ms);
+                    } else {
+                        in_bids = false;
+                        time_ms = start_time;
+                        ct.get_ask_tick_data_req(symbol_id, time_ms, to_time_ms);
+                    }
                 } else {
-                    let ts = trans_ticks(&r.tick_data);
-                    collector.asks = ts;
-
-                    // println!("{:#?}", collector);
-                    println!("{:?}", &collector.asks);
-                    println!("{:?}", &collector.bids);
-                    break;
+                    ts.iter().for_each(|v| collector.asks.push(v.clone()));
+                    if r.has_more {
+                        time_ms = ts.last().unwrap().timestamp + 1;
+                        ct.get_ask_tick_data_req(symbol_id, time_ms, to_time_ms);
+                    } else {
+                        break;
+                    }
                 }
-
-                /*                let ts = trans_ticks(&r.tick_data);
-                println!("{:#?}", ts);
-                println!("more: {:#?}", r.has_more);*/
             }
-            ResponseEvent::AssetClassListRes(_) => {}
-            ResponseEvent::SubscribeDepthQuotesRes(_) => {}
-            ResponseEvent::UnsubscribeDepthQuotesRes(_) => {}
-            ResponseEvent::SymbolCategoryListRes(_) => {}
             _ => {}
         };
     }
 
-    std::thread::sleep(std::time::Duration::new(100000, 0));
-    /* */
+    // let res = collector.final_result();
+    let res = collector.to_csv();
+    println!("{:}", res);
 }
 
 fn trans_ticks(arr: &Vec<pb::TickData>) -> Vec<pb::TickData> {
@@ -115,6 +108,7 @@ fn trans_ticks(arr: &Vec<pb::TickData>) -> Vec<pb::TickData> {
     }
 }
 
+//dep
 fn trans_ticks2(arr: &Vec<pb::TickData>) -> Vec<BTickData> {
     vec![]
 }
@@ -152,12 +146,130 @@ impl Collector {
         assert!(start);
         assert!(end);
     }
+
+    pub fn to_csv(&mut self) -> String {
+        let arr = self.final_result();
+        let res_str = helper::to_csv_out(&arr);
+        res_str
+    }
+
+    pub fn final_result(&mut self) -> Vec<BTickData> {
+        self.sort();
+        // self.assert_data();
+        self.to_bticks()
+    }
+
+    pub fn to_bticks(&self) -> Vec<BTickData> {
+        let mut arr = vec![];
+        self.bids.iter().for_each(|v| {
+            arr.push(TransTickData {
+                timestamp: v.timestamp,
+                bid_price: v.tick,
+                ask_price: 0,
+            })
+        });
+        self.asks.iter().for_each(|v| {
+            arr.push(TransTickData {
+                timestamp: v.timestamp,
+                bid_price: 0,
+                ask_price: v.tick,
+            })
+        });
+
+        arr.sort_by(|a, b| a.timestamp.cmp(&b.timestamp));
+
+        let mut tdt_arr = vec![];
+        let mut last_bid = 0;
+        let mut last_ask = 0;
+        for t in arr.iter() {
+            if t.bid_price > 0 {
+                last_bid = t.bid_price;
+            }
+            if t.ask_price > 0 {
+                last_ask = t.ask_price;
+            }
+
+            if last_bid == 00 || last_ask == 0 {
+                continue;
+            }
+
+            tdt_arr.push(TransTickData {
+                timestamp: t.timestamp,
+                bid_price: last_bid,
+                ask_price: last_ask,
+            });
+        }
+
+        // @Later: Should wer reduce ticks counts
+        let mut tdt_arr2 = vec![];
+        let mut last_time = 0;
+        // reverse as the reverse has the correct updated ticks for both bid and asks
+        tdt_arr.reverse();
+        for t in tdt_arr {
+            if t.timestamp == last_time {
+                continue;
+            }
+            last_time = t.timestamp;
+            tdt_arr2.push(t);
+        }
+        tdt_arr2.reverse();
+
+        let mut arr_res = vec![];
+        for t in tdt_arr2.iter() {
+            let bt = BTickData {
+                date_str: helper::to_time_string(t.timestamp / 1000),
+                timestamp: format!("{}_{:0<3}", t.timestamp / 1000, t.timestamp % 1000),
+                bid_price: t.bid_price as f64 / 100_000.,
+                ask_price: t.ask_price as f64 / 100_000.,
+            };
+
+            arr_res.push(bt);
+        }
+
+        arr_res
+    }
+    /*
+    pub fn to_finaliz_old(&self) {
+        let mut arr = vec![];
+        let first_ask = self.asks.first().unwrap();
+        // let first_bid = self.asks.iter().find_or_first(|v| v.timestamp >= first_ask.timestamp).;
+        let mut is_first_set = false;
+        for t in &self.asks {
+            if !is_first_set {
+                if t.timestamp >= first_ask.timestamp {
+                    let btd = BTickData::build(t.timestamp, first_ask.tick, t.tick);
+                    arr.push(btd);
+                    is_first_set = true;
+                }
+            } else {
+            }
+        }
+    }*/
 }
 
 #[derive(Debug)]
+struct TransTickData {
+    pub timestamp: i64,
+    pub bid_price: i64,
+    pub ask_price: i64,
+}
+
+#[derive(Debug, serde::Serialize)]
 pub struct BTickData {
     pub date_str: String,
-    pub timestamp: i64,
+    pub timestamp: String,
     pub bid_price: f64,
     pub ask_price: f64,
 }
+
+/*impl BTickData {
+    fn build(timestamp: i64, bid: i64, ask: i64) -> Self {
+        Self {
+            date_str: "xx".to_string(),
+            timestamp,
+            bid_price: bid as f64 / 100_000.,
+            ask_price: ask as f64 / 100_000.,
+        }
+    }
+}
+*/
--- src/helper/mod.rs
+++ src/helper/mod.rs
@@ -1,3 +1,5 @@
+use chrono::NaiveDateTime;
+use serde::Serialize;
 use std::time::{SystemTime, UNIX_EPOCH};
 
 pub fn get_time_sec() -> u64 {
@@ -16,4 +18,26 @@ pub fn get_time_ms() -> u64 {
         .expect("Time went backwards");
     // println!("{:?}", since_the_epoch);
     since_the_epoch.as_secs() * 1000
+}
+
+pub fn to_csv_out<T: Serialize>(arr: &Vec<T>) -> String {
+    let mut str_out = vec![];
+    let mut wtr = csv::Writer::from_writer(&mut str_out);
+
+    for v in arr {
+        wtr.serialize(v);
+    }
+
+    let s = wtr.flush();
+    drop(wtr);
+
+    let s = String::from_utf8(str_out).unwrap();
+
+    format!("{:}", s)
+}
+
+pub fn to_time_string(time_sec: i64) -> String {
+    let open_time = NaiveDateTime::from_timestamp(time_sec, 0);
+    let ots = open_time.format("%Y-%m-%d %H:%M:%S").to_string();
+    ots
 }
