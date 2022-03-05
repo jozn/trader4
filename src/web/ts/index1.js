3a3468a1b67ac3a7d16ae2d2afe30f4b88b109b5
Hamid <7atashfeshan@gmail.com>
2022-03-05 21:48:34 +0330

    Add typescript to web folder

---
+++ src/web/ts/index1.js
@@ -0,0 +1,17 @@
+"use strict";
+exports.__esModule = true;
+var lightweight_charts_1 = require("lightweight-charts");
+var chart = lightweight_charts_1.createChart(document.body, { width: 400, height: 300 });
+var lineSeries = chart.addLineSeries();
+lineSeries.setData([
+    { time: '2019-04-11', value: 80.01 },
+    { time: '2019-04-12', value: 96.63 },
+    { time: '2019-04-13', value: 76.64 },
+    { time: '2019-04-14', value: 81.89 },
+    { time: '2019-04-15', value: 74.43 },
+    { time: '2019-04-16', value: 80.01 },
+    { time: '2019-04-17', value: 96.63 },
+    { time: '2019-04-18', value: 76.64 },
+    { time: '2019-04-19', value: 81.89 },
+    { time: '2019-04-20', value: 74.43 },
+]);
